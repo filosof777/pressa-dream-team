ee7d56d637931035985bd2181b88742bf190b4e4
filosof777 <olimjonkamalov@mail.ru>
2022-02-05 00:30:27 +0500

    initial commit

---
+++ src/Auth/pages/Post/Post.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+
+  form {
+    
+    div {
+      display: flex;
+      flex-direction: column
+    }
+  }
+
+`
+
+export {Wrapper}
